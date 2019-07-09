6a1549c6759cde4bc4dc1c6a22f29387c01e2597
James Sutton <james@algorithmia.com>
2019-07-09 14:19:01 -0300

    replaced output file writing with try resources pattern

--- src/main/java/com/algorithmia/development/ResponseHandler.java
+++ src/main/java/com/algorithmia/development/ResponseHandler.java
@@ -10,30 +10,24 @@ final class ResponseHandler {
 
 
 
-    PrintStream preparePipe(){
-            try {
-                FileOutputStream stream  = new FileOutputStream(this.FIFOPATH, true);
-                return new PrintStream(stream, true);
-            } catch (FileNotFoundException e) {
-                throw new RuntimeException(e);
-            }
+    private void write(String serializedData){
+        try(PrintStream stream = new PrintStream(new FileOutputStream(this.FIFOPATH, true))){
+            stream.println(serializedData);
+            stream.flush();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     <OUTPUT> void writeToPipe(OUTPUT outputObject) {
         Response response = new Response(outputObject);
         String serialized = response.getJsonOutput();
-        PrintStream output = preparePipe();
-        output.println(serialized);
-        output.flush();
-        output.close();
+        write(serialized);
     }
 
     <ERRORTYPE extends RuntimeException> void writeErrorToPipe(ERRORTYPE e) {
         SerializableException<ERRORTYPE> exception = new SerializableException<>(e);
         String serialized = exception.getJsonOutput();
-        PrintStream output = preparePipe();
-        output.println(serialized);
-        output.flush();
-        output.close();
+        write(serialized);
     }
 }
